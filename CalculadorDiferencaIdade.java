0e3365adc5812f5890b0c966848399be66004a41
Isaac <luzisaac24@gmail.com>
2026-03-24 21:40:21 -0300

    Aula 11

---
+++ CalculadorDiferencaIdade.java
@@ -0,0 +1,8 @@
+public class CalculadorDiferencaIdade {
+      public static void main(String[] args) {
+            int primeiraIdade = 2620;
+            int segundaIdade = 26;
+            int diferencaIdades = primeiraIdade - segundaIdade;
+            System.out.println("A diferença entre as idades é " + diferencaIdades);
+      }
+}
