910ac695a8b361af31d44f8f8009e3ff8e42172f
Isaac <luzisaac24@gmail.com>
2026-03-25 21:32:11 -0300

    ident

---
+++ JavaJiraya/Aula03TiposPrimitivos.java
@@ -0,0 +1,14 @@
+public class Aula03TiposPrimitivos {
+    public static void main(String[] args) {
+        String nome = "Felipe";
+        String endereco = "Ceilândia";
+        double salario = 5000;
+        String dataRecebimentoSalario = "01/01/2001";
+
+        System.out.println("Eu " + nome + " morando em " + endereco + " confirmo o salário de " + salario + " no dia " + dataRecebimentoSalario);
+
+        String tudoJunto = "Eu " + nome + " morando em " + endereco + " confirmo o salário de " + salario + " no dia " + dataRecebimentoSalario;
+        
+        System.out.println( tudoJunto);
+    }
+}
---
+++ exercicios/VerificaIdade.java
@@ -0,0 +1,10 @@
+public class VerificaIdade {
+      public static void main(String[] args) {
+            float idade = 27;
+            if(idade >= 18) {
+                  System.out.println("Sua idade é " + idade );
+            } else {
+                  System.out.println("Sua idade é " + idade );
+            }
+      }
+}
